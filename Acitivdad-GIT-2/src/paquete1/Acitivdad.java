36f329640e80c92fcddb2886c5efad943366c52a
MarcCunillera <marccunillera2006@gmail.com>
2024-12-18 19:10:46 +0100

    Array con numeros aleatorios
    He hecho una array y la he llenado con números aleatorios del 1 al 100 y a continuación la printo por pantalla

--- Acitivdad-GIT-2/src/paquete1/Acitivdad.java
+++ Acitivdad-GIT-2/src/paquete1/Acitivdad.java
@@ -1,10 +1,29 @@
 package paquete1;
 
+import java.util.Random;
+
 public class Acitivdad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+ 
+        Random random = new Random();
 
+        int[][] matriz = new int[5][5];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                matriz[i][j] = random.nextInt(100) + 1; 
+            }
+        }
+        
+        System.out.println("Matriz de 5x5 con números aleatorios:");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matriz[i][j] + "\t"); 
+            }
+            System.out.println(); 
+        }
 	}
 
 }
