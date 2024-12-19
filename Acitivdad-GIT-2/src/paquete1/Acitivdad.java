13a4e9523ee7f7c5524bd265dd3abb74aa7efea7
PolPlaza <polplaza44@gmail.com>
2024-12-19 15:51:26 +0100

    Busqeda de numeros

--- Acitivdad-GIT-2/src/paquete1/Acitivdad.java
+++ Acitivdad-GIT-2/src/paquete1/Acitivdad.java
@@ -1,12 +1,13 @@
 package paquete1;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Acitivdad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- 
+        Scanner s = new Scanner(System.in);
         Random random = new Random();
 
         int[][] matriz = new int[5][5];
@@ -24,6 +25,29 @@ public class Acitivdad {
             }
             System.out.println(); 
         }
+        
+        System.out.print("Ingrese un número para buscar en la matriz: ");
+        int numeroBuscado = s.nextInt();
+        
+        boolean encontrado = false;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (matriz[i][j] == numeroBuscado) {
+                    System.out.println("Número encontrado en la posición: Fila " + i + ", Columna " + j);
+                    encontrado = true;
+                    break;
+                }
+            }
+            if (encontrado) {
+                break;
+            }
+        }
+        
+        if (!encontrado) {
+            System.out.println("El número no existe en la matriz.");
+        }
+
+        
 	}
 
 }
